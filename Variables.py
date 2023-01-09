22d0f17bedbe392dbc1825a7b134d965d986287b
EC2 Default User <ec2-user@ip-172-31-7-214.ec2.internal>
2023-01-09 14:51:28 +0000

    Variables

---
+++ Variables.py
@@ -0,0 +1,30 @@
+#A variable is a value that can change. 
+#They can be used to store information that can be referenced and used by programs. 
+#Instead of using the stored value directly, we can use the variable instead.
+#Variables could be any number of things, such as, a string of text, a list or an object. 
+#Using variables makes our code flexible and reusable. Without variables we would have to hard code all our values into the code.
+
+
+shopping_list = ("meat, eggs, bread")
+print(shopping_list)
+
+
+Colors = ("red, brown, green, yellow, orange, black")
+print(Colors)
+
+
+Cities = ("Atlanta, Los Angeles, Chicago, New York, Phoenix")
+print(Cities)
+
+Numbers = ("One, Seven, Eleven, Four, Five, Eight")
+print(Numbers)
+
+Oceans = ("Indian, Pacific, Atlantic, Artic")
+print(Oceans)
+
+
+Brandon = 1985
+print(Brandon)
+
+Months = ("January, Feburary, March, April, May, June, July")
+print(Months)
